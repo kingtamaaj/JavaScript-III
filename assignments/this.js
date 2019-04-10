fc6d01af09b846c5d303af670edea9e53da20e60
kingtamaaj <kingtamaaj@gmail.com>
2019-04-10 18:15:24 -0400

    Fixed Principle 3

--- assignments/this.js
+++ assignments/this.js
@@ -3,7 +3,7 @@
 *
 * 1. Window/Global Object Binding - This will call the window's Object not any specific object.
 * 2. Implicit Binding - The object used before the dot is this (new.this) in when a function is called.
-* 3. New Binding
+* 3. New Binding - An object creator; which allows a function to return an object.
 * 4. Explicit Binding
 *
 * write out a code example of each explanation above
@@ -13,19 +13,59 @@
 
 // code example for Window Binding
 
-function colorBlue(Blue) {
-    console.log(this);
-    return blue;
-  }
-  colorBlue("Royal"); 
+// function colorBlue(Blue) {
+//     console.log(this);
+//     return blue;
+//   }
+//   colorBlue("Royal"); 
 
 // Principle 2
 
 // code example for Implicit Binding
 
+// const myFruit = {
+//     fruit: 'Bananas',
+//     whichOne: function(name) {
+//       console.log(`${this.fruit} are my favorite ${name}`);
+//       console.log(this);
+//     }
+//   };
+//   myFruit.whichOne('fruit'); 
+
+// const fruitFunc = obj => {
+//     obj.myFruit = function() {
+//       console.log(`My favorite fruit is ${this.name}`);
+//       console.log(this);
+//     };
+//   };
+//   const mine = { name: 'Grapes' };
+//   const yours = { name: 'Banana' };
+//   fruitFunc(mine);
+//   fruitFunc(yours);
+  
+//   // Invoke Methods on our objects
+//   mine.myFruit();
+//   yours.myFruit();
+
+
 // Principle 3
 
 // code example for New Binding
+function MeanPerson(response) {
+    this.rudeGuy = 'Do not talk to me, ';
+    this.response = response;
+    this.speak = function() {
+      console.log(this.rudeGuy + this.response);
+      console.log(this);
+    };
+  }
+  
+  const bob = new MeanPerson('Bob');
+  const tom = new MeanPerson('Tom');
+  
+  bob.speak();
+  tom.speak();
+
 
 // Principle 4
 
