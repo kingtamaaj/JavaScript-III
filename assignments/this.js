f661456983c2afc458d67177d63435283732847d
kingtamaaj <kingtamaaj@gmail.com>
2019-04-10 18:03:37 -0400

    Answering questions and completing code examples in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding - This will call the window's Object not any specific object.
+* 2. Implicit Binding - The object used before the dot is this (new.this) in when a function is called.
+* 3. New Binding
+* 4. Explicit Binding
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +12,12 @@
 // Principle 1
 
 // code example for Window Binding
+
+function colorBlue(Blue) {
+    console.log(this);
+    return blue;
+  }
+  colorBlue("Royal"); 
 
 // Principle 2
 
